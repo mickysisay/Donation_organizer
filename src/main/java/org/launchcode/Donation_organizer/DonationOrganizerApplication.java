33b284eb2d4ce7fe4871cec0781f00b745c7a44a
Michael Sisay <ashenafimichael25@gmail.com>
2020-02-19 02:22:06 -0600

    "project started"

---
+++ src/main/java/org/launchcode/Donation_organizer/DonationOrganizerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Donation_organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonationOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/Donation_organizer/DonationOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Donation_organizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
